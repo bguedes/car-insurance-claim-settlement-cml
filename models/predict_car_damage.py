3d368c4f1e8397f2ce9c67ec72d81852500c43a8
bguedes <guedes.bruno@gmail.com>
2022-06-09 15:38:26 +0200

    Update predict_car_damage.py

--- models/predict_car_damage.py
+++ models/predict_car_damage.py
@@ -25,7 +25,7 @@ def pred_and_plot(model, base64ImageEncoded):
   else:
       return {"car_damaged": "false"}
 
-def detectIfCarIsImage():
+def detectIfCarIsDamaged():
     imageBase64Encoded = args["imageBase64"]
     model = keras.models.load_model('car_damage_or_not_detection_model.h5')
     return pred_and_plot(model, imageBase64Encoded)
